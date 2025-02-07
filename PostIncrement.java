0032b58101ee3c91c5a7f98ad3a7eb2d7012c3fb
Abhishek <abhisheksen77777@gmail.com>
2025-02-08 02:28:42 +0530

    feb 8

---
+++ PostIncrement.java
@@ -0,0 +1,8 @@
+public class PostIncrement {
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println(++i);
+        System.out.println(i++);
+        System.out.println(++i);
+    }
+}
