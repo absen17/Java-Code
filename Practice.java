6eaa988512aec6bd55c8f0863d48511e89b8fe54
Abhishek <abhisheksen77777@gmail.com>
2025-02-09 21:51:08 +0530

    feb 9

---
+++ Practice.java
@@ -0,0 +1,13 @@
+public class Practice {
+
+    public int getSum(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    public static void main(String args[]) {
+
+        Practice obj = new Practice();
+        System.out.println(obj.getSum());
+    }
+}
