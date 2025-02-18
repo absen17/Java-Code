1aada7dae85405ab7e88ba672c032ec1287c82b2
Abhishek <abhisheksen77777@gmail.com>
2025-02-18 21:41:25 +0530

    Feb 18

---
+++ P2.java
@@ -0,0 +1,8 @@
+public class P2 {
+    public static void main(String[] args) {
+            int x = 15;
+            int y = x++;
+            int  z = ++x;
+            System.out.println(y +" " + z);
+    }
+}
