805f67df925e5a81e03cd96f0ef93853c24836f3
Abhishek Sen <abhisheksen77777@gmail.com>
2025-05-28 23:09:51 +0530

    Comaparator Practice

---
+++ DemoComparator2.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class DemoComparator2 {
+    public static void main(String[] args) {
+
+        Comparator<String> com = (a,b)->a.length()>b.length()?1:-1;
+        
+        List<String> name = new ArrayList<>();
+
+        name.add("Abhishek");
+        name.add("Sen");
+        name.add("Pujan");
+        name.add("Ab");
+
+        Collections.sort(name,com);
+
+        for(String n : name){
+            System.out.print(n + " ");
+        }
+        
+
+    }
+}
