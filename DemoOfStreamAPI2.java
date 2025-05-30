28f60ec1a3b380113985d5c86d457b3a7a9f2b8c
Abhishek Sen <abhisheksen77777@gmail.com>
2025-05-30 21:41:39 +0530

    Stream API

---
+++ DemoOfStreamAPI2.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class DemoOfStreamAPI2 {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,12,44,33,33,4,1,4);
+        Set<Integer> collect = list.stream()
+                                .filter(i->Collections.frequency(list, i)>1)
+                                .collect(Collectors.toSet());
+        System.out.println(collect);
+    }   
+}
