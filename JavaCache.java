ae2626e6d46dba3c57e3d15ca01e21ae17e72992
Abhishek Sen <abhisheksen77777@gmail.com>
2025-06-13 19:45:18 +0530

    java cache

---
+++ JavaCache.java
@@ -0,0 +1,12 @@
+public class JavaCache {
+    public static void main(String[] args) {
+        
+        Integer one = Integer.valueOf(100);
+        Integer two = Integer.valueOf(100);
+        System.out.println(one == two); // true
+
+        Integer three = Integer.valueOf(300);
+        Integer four = Integer.valueOf(300);
+        System.out.println(three == four); // false
+    }
+}
