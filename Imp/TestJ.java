9487f0b6058fd86838b11132c38b3ea2e63279cb
Abhishek Sen <abhisheksen77777@gmail.com>
2025-06-24 13:24:49 +0530

    main overload

--- Imp/TestJ.java
+++ Imp/TestJ.java
@@ -1,9 +1,9 @@
 package Imp;
 public class TestJ{
     public static void main(String[] args) {
-       
+       main(1);
     }
-    public static void main(String[] args,int a){
-        System.out.println();
+    public static void main(int a){
+        System.out.println("hello");
     }
 }
